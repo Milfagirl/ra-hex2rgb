4ee21a1d3fcdc7b509059a34b5b89a50d0987cdb
Svetlana Ryabova <ganyim@inbox.ru>
2020-11-27 16:59:29 +0500

    check color

--- src/App.js
+++ src/App.js
@@ -5,33 +5,47 @@ import './App.css';
 function App() {
   const [inputValue, setInpitValue] = useState('');
   const [divValue, setDivValue] = useState('');
-  const [bgkColor, setBgkColor] = useState({ backgroundColor: "#ff" })
+  const [bgkColor, setBgkColor] = useState({ backgroundColor: "#FFFFFF" })
 
   const onChangeInput = (event) => {
     setInpitValue(prev => event.target.value);
+    if (event.target.value.length === 7) {
+      Convert(event.target.value);
+    } else {
+      setDivValue(prev => '');
+      setBgkColor(prev => ({ ...prev, backgroundColor: "#FFFFFF" }));
+    }
+
+
   }
 
-  const onSubmitForm = (e) => {
-    e.preventDefault();
-    if (/^#([0-9a-f]{3}|[0-9a-f]{6})$/ig.test(inputValue)) {
-      var hex = inputValue.substr(1);
+  const Convert = (value) => {
+    if (/^#([0-9a-f]{3}|[0-9a-f]{6})$/ig.test(value)) {
+
+      var hex = value.substr(1);
+      console.log(hex);
       hex = hex.length === 3 ? hex.replace(/(.)/g, '$1$1') : hex;
       var rgb = parseInt(hex, 16);
       let rgbstr = 'rgb(' + [(rgb >> 16) & 255, (rgb >> 8) & 255, rgb & 255].join(',') + ')';
       setDivValue(prev => rgbstr);
-      setBgkColor(prev => ({ ...bgkColor, backgroundColor: rgbstr }))
+      setBgkColor(prev => ({ ...prev, backgroundColor: rgbstr }));
     }
-    setDivValue(prev => 'Ошибка');
+    else {
+      setDivValue(prev => 'Ошибка');
+      setBgkColor(prev => ({ ...prev, backgroundColor: "#FFFFFF" }));
+    }
+
+
   }
 
 
 
   return (
     <div style={bgkColor} className="App">
-      <form onSubmit={onSubmitForm}>
+      <div id='forInput'>
         <input className='hex' id="name" name="name" value={inputValue} onChange={onChangeInput} />
         <div className='rgb'>{divValue}</div>
-      </form>
+      </div>
     </div>
   );
 }
